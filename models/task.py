fac5201c39bb57a3947071caa94c6c26577fa082
ITLAZ <juan.tapia.l@ucb.edu.bo>
2024-02-22 22:51:19 -0400

    funciones API

---
+++ models/task.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class Task(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(default="Tarea Nueva", min_length=3, max_length=50)
+    description: Optional[str] = Field(default="Detalles de la tarea", max_length=250)
+    status: str = Field(default="Pendiente", max_length=20)
+    created_at: Optional[str] = Field(default=date.today().strftime("%d-%m-%Y"))
+    due_date: str = None
